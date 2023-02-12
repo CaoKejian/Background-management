f6fcaa168de05abe7a954b677a0d89ec7397f158
曹珂俭 <1849201815@qq.com>
2023-02-12 22:35:28 +0800

    封装登陆状态条及一些路由动画

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import {createVNode,render} from 'vue'
+import  loadingBar  from '../components/loadingBar.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import ('../views/login/login.vue')
+      component: () => import ('../views/login/login.vue'),
+      meta:{
+        title:"登陆页面",
+        transition:"animate__fadeIn"
+      }
     },
   ]
 })
 
+
+const setTitle = (to:RouteLocationNormalized) => {
+  if(to.meta.title == undefined){
+    document.title ='还不知道叫什么后台'
+  }else{
+    document.title = to.meta.title
+  }
+}
+const Vnode = createVNode(loadingBar)
+render(Vnode,document.body)
+router.beforeEach((to, from, next) => {
+  Vnode.component?.exposed?.startLoading()
+  setTitle(to)
+  next()
+});
+router.afterEach((to,from)=>{
+  Vnode.component?.exposed?.endLoading()
+})
 export default router
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,5 @@
-<template>
-  <div>
+<template #default="{route,Component}">
+  <div :class="isshow !== 1 ? 'active' : 'wrapper' && `animate__animated ${$route.meta.transition}`">
     <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm">
       <el-form-item prop="username">
         <el-input v-model="ruleForm.username" type="text" autocomplete="off" />
@@ -11,11 +11,19 @@
         <el-button type="primary" @click="submitForm()">Submit</el-button>
       </el-form-item>
     </el-form>
+
+    <div id="cssLoader17" class="main-wrap main-wrap--white " v-show="isshow !== 1"
+      :class="`animate__animated ${$route.meta.transition}`">
+      <div class="cssLoader17"></div>
+    </div>
+
   </div>
 </template>
 <script setup lang='ts'>
 import { ref, reactive, toRefs } from 'vue'
 import { adminLoginApi } from '@/request/api'
+import './login.css'
+import 'animate.css';
 type Loginitf = {
   username: string
   pwd: string
@@ -28,6 +36,7 @@ const state = reactive({
 })
 let { ruleForm } = toRefs(state)
 let ruleFormRef = ref()
+const isshow = ref(1)
 const validatePwd = (rule: unknown, value: string | undefined, cb: (msg?: string) => void) => {
   if (!value) {
     cb('密码不能为空')
@@ -46,6 +55,7 @@ const rules = reactive({
 })
 
 const submitForm = () => {
+  // isshow.value = 2
   ruleFormRef.value.validate().then(() => {
 
   }).catch(() => {
