76f9beb1b6580d2d28f1a7cbdc6a15516413ec15
曹珂俭 <1849201815@qq.com>
2023-02-13 20:38:45 +0800

    首页菜单渲染

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -7,7 +7,9 @@ interface MenuObj {
   children?:MenuObj[]
   name:string
 }
-
+type NewMenus = {
+  [key:number]:MenuObj
+}
 export const useInfoStore = defineStore('info', {
   state:()=>{
     return{
@@ -17,7 +19,22 @@ export const useInfoStore = defineStore('info', {
     }
   },
   getters:{
-
+    getNewMenus(state){
+      const newMenus:NEwMenus = {}
+      const menu = state.menu
+      for (let i = 0; i < menu.length; i++) {
+        if(menu[i].parentId === 0){
+          // 一级菜单
+          newMenus[menu[i].id] = {...menu[i],children:newMenus[menu[i].id]?.children || []} 
+        }else{
+          let parentId =  menu[i].parentId
+          newMenus[parentId] = newMenus[parentId] || {}
+          newMenus[parentId].children = newMenus[parentId].children || []
+          newMenus[parentId].children?.push(menu[i])
+        }
+      }
+      return newMenus
+    }
   },
   actions:{
 
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,75 @@
 <template>
-  <div>
-    home
+  <div class="container" :class="`animate__animated ${$route.meta.transition}`">
+    <div class="header">头部</div>
+    <div class="menu">
+      <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" default-active="2"
+        text-color="#fff" @open="handleOpen" @close="handleClose" :unique-opened="true" :router="true">
+        <el-sub-menu :index="menus.id + ''" v-for="menus in  newMenus" :key="menus.id">
+          <template #title>
+            <span>{{ menus.title }}</span>
+          </template>
+          <template v-for="submenu in menus.children" :key="submenu.id">
+            <el-menu-item :index="'/' + menus.name + '/' + submenu.name" v-if="!submenu.hidden">{{
+              submenu.title
+            }}</el-menu-item>
+          </template>
+        </el-sub-menu>
+      </el-menu>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <script setup lang='ts'>
 import { ref } from 'vue'
+import { useInfoStore } from '@/stores/counter'
+const useInfo = useInfoStore()
 
+interface MenuObj {
+  parentId: number
+  id: number
+  hidden?: 0 | 1
+  title?: string
+  children?: MenuObj[]
+  name?: string
+}
+type NewMenus = {
+  [key: number]: MenuObj
+}
+
+const newMenus: NewMenus = useInfo.getNewMenus
+const handleOpen = (key: string, keyPath: string[]) => {
+}
+const handleClose = (key: string, keyPath: string[]) => {
+}
 </script>
 <style lang='less' scoped>
+.container {
+  height: 100vh;
+  position: relative;
 
+  .header {
+    height: 4.375rem;
+    background-color: pink;
+  }
+
+  .menu {
+    position: absolute;
+    width: 15.625rem;
+    top: 4.375rem;
+    left: 0;
+    bottom: 0;
+    background-color: rgb(47, 47, 129);
+  }
+
+  .content {
+    position: absolute;
+    top: 4.375rem;
+    right: 0;
+    left: 15.625rem;
+    bottom: 0;
+    background-color: skyblue;
+  }
+}
 </style>
