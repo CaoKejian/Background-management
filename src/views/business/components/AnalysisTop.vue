4a1732cdd29bcdb527771311f6ceeb27ab57ba2a
曹珂俭 <1849201815@qq.com>
2023-02-20 13:33:17 +0800

    AnalysisTop设计完毕

--- src/views/business/components/AnalysisTop.vue
+++ src/views/business/components/AnalysisTop.vue
@@ -4,14 +4,24 @@
       <div class="content">
         <div class="box1">
           <span>总销售额</span>
-          <span>总销售额</span>
+          <el-tooltip class="box-item" effect="dark" content="指标说明" placement="top">
+            <el-icon>
+              <WarningFilled />
+            </el-icon>
+          </el-tooltip>
         </div>
         <div class="box2">
           ￥ 126,560
         </div>
         <div class="box3">
-          <span>周同比 12%</span>
-          <span>日同比 11%</span>
+          <div>
+            <span>周同比 12%<el-icon>
+                <CaretTop color="#f5222d" />
+              </el-icon></span>
+            <span>日同比 11%<el-icon>
+                <CaretBottom color="#52c41a" />
+              </el-icon></span>
+          </div>
         </div>
         <div class="box4">
           <span>日销售额 ￥ 12,423 </span>
@@ -21,52 +31,65 @@
     <div class="box-top">
       <div class="content">
         <div class="box1">
-          <span>总销售额</span>
-          <span>总销售额</span>
+          <span>访问量</span>
+          <el-tooltip class="box-item" effect="dark" content="指标说明" placement="top">
+            <el-icon>
+              <WarningFilled />
+            </el-icon>
+          </el-tooltip>
         </div>
         <div class="box2">
-          ￥ 126,560
+          8,846
         </div>
         <div class="box3 box-line">
         </div>
         <div class="box4">
-          <span>日销售额 ￥ 12,423 </span>
+          <span>日访问量 1,234 </span>
         </div>
       </div>
     </div>
     <div class="box-top">
       <div class="content">
         <div class="box1">
-          <span>总销售额</span>
-          <span>总销售额</span>
+          <span>支付笔数</span>
+          <el-tooltip class="box-item" effect="dark" content="指标说明" placement="top">
+            <el-icon>
+              <WarningFilled />
+            </el-icon>
+          </el-tooltip>
         </div>
         <div class="box2">
-          ￥ 126,560
+          6,560
         </div>
-        <div class="box3">
-          <span>周同比 12%</span>
-          <span>日同比 11%</span>
-        </div>
+        <div class="box3 box-zhu"></div>
         <div class="box4">
-          <span>日销售额 ￥ 12,423 </span>
+          <span>转换率 60% </span>
         </div>
       </div>
     </div>
     <div class="box-top">
       <div class="content">
         <div class="box1">
-          <span>总销售额</span>
-          <span>总销售额</span>
+          <span>活动效果</span>
+          <el-tooltip class="box-item" effect="dark" content="指标说明" placement="top">
+            <el-icon>
+              <WarningFilled />
+            </el-icon>
+          </el-tooltip>
         </div>
         <div class="box2">
-          ￥ 126,560
+          78%
         </div>
         <div class="box3">
-          <span>周同比 12%</span>
-          <span>日同比 11%</span>
+          <el-progress :percentage="78" :stroke-width="15" color='#1fc5c5' stroke-linecap='butt' />
         </div>
         <div class="box4">
-          <span>日销售额 ￥ 12,423 </span>
+          <span>周同比 12%<el-icon>
+              <CaretTop color="#f5222d" />
+            </el-icon></span>
+          <span>日同比 11%<el-icon>
+              <CaretBottom color="#52c41a" />
+            </el-icon></span>
         </div>
       </div>
     </div>
@@ -78,6 +101,7 @@ import * as echarts from 'echarts';
 onMounted(() => {
   setTimeout(() => {
     initLine()
+    initZhu()
   }, 0)
 })
 const initLine = () => {
@@ -88,7 +112,7 @@ const initLine = () => {
     grid: {
       //与绝对定位相似，top，left，right，bottom 设定是根据上级盒子宽高来计算
       top: "10%",
-      left: '0',
+      left: '-10%',
       right: '0',
       bottom: "10%",
     },
@@ -106,8 +130,46 @@ const initLine = () => {
         data: [1, 4, 1, 5, 2, 3, 2, 4, 2, 1.5, 3, 2, 4, 3, 2],
         type: 'line',
         showSymbol: false,
-        smooth: true
+        smooth: true,
+        areaStyle: {
+          color: '#c5a5f0'
+        },
+        lineStyle: {
+          color: '#9861e3'
+        }
       }
+    ]
+  })
+}
+const initZhu = () => {
+  const charts = echarts.init(
+    document.querySelector(".box-zhu") as HTMLElement
+  );
+  charts.setOption({
+    grid: {
+      //与绝对定位相似，top，left，right，bottom 设定是根据上级盒子宽高来计算
+      top: "10%",
+      left: '-10%',
+      right: '0',
+      bottom: "10%",
+    },
+    xAxis: {
+      show: false,
+      type: 'category',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    },
+    yAxis: {
+      show: false,
+      type: 'value'
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130, 120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        itemStyle: {
+          color: "#6395f9"
+        }
+      },
     ]
   })
 }
@@ -137,6 +199,8 @@ const initLine = () => {
       .box1 {
         color: #8c8c8c;
         font-size: 14px;
+        display: flex;
+        justify-content: space-between;
       }
 
       .box2 {
@@ -149,9 +213,11 @@ const initLine = () => {
         width: 100%;
         height: 4.5em;
         font-size: 14px;
-        line-height: 4.5em;
         border-bottom: 1px solid #efefef;
 
+        div {
+          line-height: 4.0em;
+        }
       }
 
       .box4 {
