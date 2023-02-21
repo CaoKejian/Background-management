27a2a00f19cae7240d9fe25cb15811058b210fb1
曹珂俭 <1849201815@qq.com>
2023-02-21 23:59:35 +0800

    公共div组件 传值

---
+++ src/views/business/businessMonitoring.vue
@@ -0,0 +1,26 @@
+<template #default="{route,Component}">
+  <div class='wrapper' :class="`animate__animated ${$route.meta.transition}`">
+    <AlluseDiv :name="name" />
+  </div>
+</template>
+<script setup lang='ts'>
+import { ref, reactive, toRefs } from 'vue'
+import 'animate.css';
+import AlluseDiv from './components/AlluseDiv.vue'
+
+const state = reactive<{
+  name: string
+}>({
+  name: 'ckj'
+})
+const { name } = toRefs(state)
+</script>
+<style lang='less' scoped>
+.wrapper {
+  width: 100%;
+  height: 120vh;
+  background-color: #f0f2f5;
+  background-color: skyblue;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/business/components/AlluseDiv.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="allusediv">
+    {{ propData.name }}
+  </div>
+</template>
+<script setup lang='ts'>
+import { ref } from 'vue'
+type Props = {
+  name: string
+}
+const propData = defineProps<Props>()
+</script>
+<style lang='less' scoped>
+.allusediv {
+  width: 70%;
+  background-color: red;
+}
+</style>
