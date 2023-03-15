643b2eb4a4d7c1ab2f5f4cd2b30d62ec58e7afdc
曹珂俭 <1849201815@qq.com>
2023-03-15 08:14:49 +0800

    添加商品第四个页面完成

--- src/views/product/components/indexFore.vue
+++ src/views/product/components/indexFore.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="table" v-show="PropData.visible4">
     <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm">
-      <el-form-item>
-        4
+      <el-form-item label="当前账号" prop="use">
+        <el-input v-model="ruleForm.use" type="text" autocomplete="off" />
       </el-form-item>
-
+      <el-form-item label="当前密码" prop="pwd">
+        <el-input v-model="ruleForm.pwd" type="password" autocomplete="off" />
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm()">下一步</el-button>
       </el-form-item>
@@ -22,26 +24,30 @@ const PropData = defineProps<Props>()
 const ruleFormRef = ref()
 const state = reactive<{
   ruleForm: {
+    use: string
+    pwd: string
   }
 }>({
   ruleForm: {
+    use: "admin",
+    pwd: "123456"
   }
 })
 const { ruleForm } = toRefs(state)
 
-const valiDataInte = (rele: unknown, value: number | undefined, cb: (msg?: string) => void) => {
-  if (value == null) {
-    cb('积分不能为空')
+const validatePwd = (rule: unknown, value: string | undefined, cb: (msg?: string) => void) => {
+  if (!value) {
+    cb('密码不能为空')
   } else {
-    if (value < 0) {
-      cb('积分不能小于0')
-    }
     cb()
   }
 }
 const rules = reactive({
-  integral: [
-    { validator: valiDataInte, tregger: 'blur' }
+  use: [
+    { required: true, message: "用户名不能为空", tregger: 'blur' }
+  ],
+  pwd: [
+    { validator: validatePwd, tregger: 'blur' }
   ]
 })
 const emits = defineEmits<{
